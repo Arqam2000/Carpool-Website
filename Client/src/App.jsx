1645fd19c1a934a79c656e99427c7f3c1949a393
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-08-13 03:30:15 +0500

    client

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Home from './Components/Home'
+
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/Components/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Signup.css'
+
+const Signup = () => {
+  return (
+    <>
+        <form action="">
+            <div className="name">
+                <input type="text" name="firstName" id="" />
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default Signup

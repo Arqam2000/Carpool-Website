82119997f5ce4cb8dfeb802047801cd538a813c5
Arqam2000 <arqamsarwar9@gmail.com>
2023-08-26 21:03:38 +0500

    updated car cards

--- Client/src/Components/Car Cards/Cards.jsx
+++ Client/src/Components/Car Cards/Cards.jsx
@@ -1,51 +1,37 @@
 import "./Cards.css"
-import car from "./car-1.png"
+import carImg from "./car-1.png"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-function Cards(){
-    const [users, setUsers] = useState([])
-    const authAxios = axios.create({
-        baseURL: "http://localhost:3001/car-details",
-        headers: {
-          Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`
-            }})
-    useEffect(() => {
-    
-        authAxios.get('http://localhost:3001/car-details')
-            .then(users => {
-                console.log(users.data)
-                setUsers(users.data)
-            })
-            .catch(err => console.log(err))
-    }, [])
-return(
-    <>
-    <div className="parent">
-    <div className="main-card">
-        <div className="imgd">
-      <img src={car} alt="" />
-        </div>
-        <div className="text-1">
-            <div style={{ display:"flex"}}>
-            <p>Car Name:</p>
-            <p className="ot">{users.map((car)=>car.carName)}</p>
-            </div>
-            <div>
-                <p>Car Model:</p>
-                <p>{users.carModel}</p>
-            </div>
-            <div>
-                <p></p>
-            </div>
-        </div>
-    </div>
-    </div>
+function Cards({car}) {
 
-    
-    
-    </>
-)
+    return (
+        <>
+            <div className="parent">
+                <div className="main-card">
+                    <div className="imgd">
+                        <img src={carImg} alt="" />
+                    </div>
+                    <div className="text-1">
+                        <div style={{ display: "flex" }}>
+                            <p>Car Name: </p>
+                            <p className="ot">{car.carName}</p>
+                        </div>
+                        <div style={{ display: "flex" }}>
+                            <p>Car Model: </p>
+                            <p>{car.carModel}</p>
+                        </div>
+                        <div>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+    )
 }
 
 export default Cards; 
--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -4,24 +4,40 @@ import { Navigate, useNavigate } from "react-router-dom";
 import Navbar2 from "./Navbar2";
 import "./Home.css"
 import plus from "./Images/Plus.png"
+import axios from "axios"
+import Cards from "../Car Cards/Cards";
 
 
 
 function Home() {
   const [click, setClick] = useState("ex");
+  const [cars, setCars] = useState([])
   const navigate = useNavigate()
-  
-  useEffect(()=>{
+
+  const authAxios = axios.create({
+    baseURL: "http://localhost:3001/car-details",
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`
+    }
+  })
+
+  useEffect(() => {
     if (!click) {
       navigate("/cardetailsmain")
     }
   }, [click])
- 
+
 
   useEffect(() => {
     if (!JSON.parse(localStorage.getItem("token"))) {
       navigate("/login")
     }
+    authAxios.get('http://localhost:3001/car-details')
+      .then(users => {
+        console.log(users.data)
+        setCars(users.data)
+      })
+      .catch(err => console.log(err))
   }, [])
   
   return (
@@ -29,13 +45,10 @@ function Home() {
       <div className="Navbar">
         <Navbar2 />
       </div>
-      
-      
-      
       <div>
-        <img src={plus} alt="" className="plus" onClick={() => setClick(null)}/>
+        <img src={plus} alt="" className="plus" onClick={() => setClick(null)} />
       </div>
-   
+      {cars.map(car => <Cards key={car._id} car={car} />)}
     </>
   );
 }

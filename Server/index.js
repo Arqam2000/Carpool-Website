a313f371149c8625b123e6a51772b1ce9f052c15
Arqam2000 <126051128+Arqam2000@users.noreply.github.com>
2023-08-30 14:59:00 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -3,18 +3,17 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const uri = "mongodb+srv://jedasow785:UXvgVYIEtbUwBCZA@cluster0.rkwqjvs.mongodb.net/Users?retryWrites=true&w=majority"
 const app = express()
-
-app.use(express.json())
-app.use(cors())
-app.use(express.static('public'))
-
-mongoose.connect(uri)
-
+const path = require('path')
 const route = require("./Routes/auth")
 const car_route = require("./Routes/CarDetails")
 
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use("/", route)
 app.use("/", car_route)
+
+mongoose.connect(uri)
 
 app.listen(3001,() => {
     console.log("Server is listening at port 3001")
